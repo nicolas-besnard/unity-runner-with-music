919c92b417139d248abade0ed425463d8e85ed9b
Nicolas Besnard <besnard.nicolas@gmail.com>
2013-09-27 23:35:41 +0200

    - Add "enemy" logic - Improve beast blinking - Some textures

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,6 +12,24 @@ public class Movement : MonoBehaviour
 	
 	void 				Update () 
 	{
+		float			axis;
+		
+		if ((axis = Input.GetAxis("Vertical")) != 0)
+		{
+			float y;
+			if (axis > 0)
+			{
+				y = transform.position.y + 1;
+			}
+			else
+			{
+				y = transform.position.y - 1;
+			}
+			
+			float		newY = Mathf.Clamp(y, 0.5f, 5.0f);
+			Vector3		newPos = new Vector3(transform.position.x, newY, transform.position.z);
+			transform.position = Vector3.Lerp(transform.position, newPos, speed * Time.deltaTime);
+		}
 		transform.Translate (Vector3.forward * speed * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour 
+{
+	void				OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("YOU DIE");
+		Application.LoadLevel("Game"); 
+	}
+
+}
